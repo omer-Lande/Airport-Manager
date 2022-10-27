f1f72dea0d3d04a71a2521250cf510d6fb8c9c61
omer-Lande <om3r609@gmail.com>
2022-10-27 19:28:32 +0300

    first commit

---
+++ Plane.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#define CODE_LENGTH 4
+
+typedef enum { 
+	eCommercial, eCargo, eMilitary, eNofPlaneTypes 
+} ePlaneType;
+
+
+static const char* PlaneTypeStr[eNofPlaneTypes];
+
+typedef struct
+{
+	ePlaneType	type;
+	char	code[CODE_LENGTH + 1];
+}Plane;
+
+void		initPlane(Plane* pPlane);
+ePlaneType	getPlaneType();
+void		getPlaneCode(char* code);
+void		printPlane(const Plane* pPlane);
+int			savePlaneToFile(const Plane* pPlane, FILE* fp);
+int			loadPlaneFromFile(Plane* pPlane, FILE* fp);
+
